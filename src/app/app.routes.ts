30bfd6c9a9f23aa8941c19fb2bceb4af2d665c9f
Vmateogms <vicentemateogarciamerino@gmail.com>
2025-03-06 18:36:52 +0100

    segundo commit: versión inicial del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import { Component } from '@angular/core';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductViewComponent } from './components/product-view/product-view.component';
+import { Page404Component } from './pages/page404/page404.component';
 
 export const routes: Routes = [
-
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: 'home', component: ProductListComponent},
     { path: 'producto/:_id', component: ProductViewComponent},
-    { path: '', redirectTo: '/home', pathMatch: 'full'},
-        //page404 aqui
+    { path: '**', component: Page404Component},
 
 
 
